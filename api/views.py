230adfab677ffce2c391522d67c9c65cb96a9ba3
shivShakya <shivdu2000@gmail.com>
2022-07-25 10:58:39 +0530

    First commit

---
+++ api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoGetCreate(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+class TodoUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+     queryset = Todo.objects.all()
+     serializer_class = TodoSerializer
+
+
